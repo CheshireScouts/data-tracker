b63e3826d7baaeb4490e0996216d27fecf9f68a5
Owen Campbell <owen.campbell@sempiria.co.uk>
2013-01-26 13:38:00 +0000

    fixes mass assignment problem

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,5 +2,5 @@ class Organisation < ActiveRecord::Base
   belongs_to :parent, :class_name => "Organisation"
   has_many :children, :class_name => "Organisation"
   belongs_to :organisation_type
-  attr_accessible :name
+  attr_accessible :name, :parent_id, :organisation_type_id
 end
