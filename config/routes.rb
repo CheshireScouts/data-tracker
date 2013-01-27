872ac8ce5c7b21a2e2ffe4cfe727866aaa5cb3d2
Owen Campbell <owen.campbell@sempiria.co.uk>
2013-01-27 13:38:04 +0000

    Fixes #2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ DataTracker::Application.routes.draw do
     root :to => 'organisations#index'
   end
   root :to => "home#index"
-  devise_for :users
+
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   resources :users
 end
