d6de75203e27292a53ecc882cddc195b875aa6a9
Owen Campbell <owen.campbell@sempiria.co.uk>
2013-01-28 20:41:03 +0000

    Adds pivot table membership view

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,7 +1,12 @@
 class Organisation < ActiveRecord::Base
   belongs_to :parent, :class_name => "Organisation"
   has_many :children, :class_name => "Organisation"
+  has_many :memberships, :class_name => "Membership"
   belongs_to :organisation_type
   attr_accessible :name, :parent, :parent_id, :organisation_type, :organisation_type_id
   has_ancestry
+
+  def total_head_count_for_year(year)
+  	Membership.sum(:head_count, :conditions => { :year_id => year })
+  end  	
 end
