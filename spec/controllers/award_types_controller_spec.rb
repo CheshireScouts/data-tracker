c3881c750f2d4b01876194e33dd2cf789529fb20
Owen Campbell <owen.campbell@1sthelsbyscouts.org.uk>
2013-07-12 16:17:17 +0100

    Testing poplution of array by controller

--- spec/controllers/award_types_controller_spec.rb
+++ spec/controllers/award_types_controller_spec.rb
@@ -8,9 +8,15 @@ describe AwardTypesController do
   end
 
   describe "Get index" do
+
+    it "populates an array of awards types" do
+      award_type = FactoryGirl.create(:award_type)
+      get :index
+      assigns(:award_types).should eq([award_type])
+    end
     
     it "renders the :index view" do
-      get :index, :format => "html"
+      get :index
       response.should render_template(:index)
     end
 
