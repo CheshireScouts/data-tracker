e6b011710e37ec3026c446f651a94173dc89df50
Owen Campbell <owen.campbell@1sthelsbyscouts.org.uk>
2013-07-12 11:56:15 +0100

    Coverage now working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
+require 'simplecov'
+SimpleCov.start 'rails'
 
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start 'rails'
-end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
