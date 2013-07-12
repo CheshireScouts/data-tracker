46d000eddb3a442a2b25bbcfd351e402e109b320
Owen Campbell <owen.campbell@1sthelsbyscouts.org.uk>
2013-07-12 16:28:04 +0100

    Adds test for show method

--- spec/controllers/award_types_controller_spec.rb
+++ spec/controllers/award_types_controller_spec.rb
@@ -8,18 +8,29 @@ describe AwardTypesController do
   end
 
   describe "Get index" do
-
     it "populates an array of awards types" do
       award_type = FactoryGirl.create(:award_type)
       get :index
       assigns(:award_types).should eq([award_type])
     end
     
-    it "renders the :index view" do
+    it "renders the index view" do
       get :index
-      response.should render_template(:index)
+      response.should render_template :index
+    end
+  end
+
+  describe "Get show" do
+    it "assigns the requested award type" do
+      award_type = FactoryGirl.create(:award_type)
+      get :show, id: award_type
+      assigns(:award_type).should eq(award_type)
     end
 
+    it "renders the show view" do
+      get :show, id: FactoryGirl.create(:award_type)
+      response.should render_template :show
+    end
   end
 
 end
