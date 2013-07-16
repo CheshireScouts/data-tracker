4ca27d2435a60b70c819fc493d6aea314aa65ed9
Owen Campbell <owen.campbell@1sthelsbyscouts.org.uk>
2013-07-16 14:22:46 +0100

    Ads organisation types controller spec

--- spec/factories/organisation_types.rb
+++ spec/factories/organisation_types.rb
@@ -2,4 +2,8 @@ FactoryGirl.define do
   factory :organisation_type do |f|
     f.name {Faker::Name.name}
   end
+
+  factory :invalid_organisation_type, parent: :organisation_type do |f|
+    f.name nil
+  end
 end
